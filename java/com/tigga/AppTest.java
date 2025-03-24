d3d93ecee660da3497740c377b7e8bbd382a7243
Sunil tigga <sangeetatigga2604@gmail.com>
2025-03-24 11:31:38 +0530

    Add files via upload

---
+++ java/com/tigga/AppTest.java
@@ -0,0 +1,15 @@
+package com.tigga;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true, "This test should always pass.");
+    }
+}
